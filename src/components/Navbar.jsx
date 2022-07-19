b6af11fd5f389b8f9b3297bed71d26db6bdbe686
Irfan Maulana <irfan@Mazzels-MacBook-Pro.local>
2022-07-19 23:13:41 +0700

    starter setup

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='m-8'>
+        <h1 className='text-white text-3xl'>NETFLIX</h1>
+    </div>
+  )
+}
+
+export default Navbar
