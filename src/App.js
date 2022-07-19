6a3972fd449c2692cb642d8876b314f43eb30aab
Irfan Maulana <irfan@Mazzels-MacBook-Pro.local>
2022-07-20 00:03:31 +0700

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import { Route, Router } from 'react-router-dom';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <>
       <Navbar />
+      <Router>
+        <Route path='/' component={<Home />} />
+      </Router>
     </>
   );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,8 +2,12 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div className='m-8'>
-        <h1 className='text-white text-3xl'>NETFLIX</h1>
+    <div className='p-4 flex items-center justify-between z-[100] w-full absolute'>
+        <h1 className='text-red-600 text-4xl font-bold cursor-pointer'>NETFLIX</h1>
+        <div>
+          <button className='text-white pr-4'>Sign In</button>
+          <button className='bg-red-600 px-6 py-2 rounded text-white'>Sign Up</button>
+        </div>
     </div>
   )
 }
